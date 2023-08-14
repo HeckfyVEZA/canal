03995c2eca385f41697a6d5fe550c9ffecfdc94e
HeckfyVEZA <139766140+HeckfyVEZA@users.noreply.github.com>
2023-08-14 12:30:35 +0300

    Update info_search.py

--- info_search.py
+++ info_search.py
@@ -81,8 +81,8 @@ def addon(addons):
             ado['частотник/регулятор'] = True
         elif ': ' in item and len(findall(r"(\d+) ?шт\.?", item)):
             ado['addons'].append([c_str(item.split(': ')[1].split()[0]), findall(r"(\d+) ?шт\.?", item)[0] if len(findall(r"(\d+) ?шт\.?", item)) else 1])
-        else:
-            ado['частотник/регулятор'] = False
+        # else:
+        #     ado['частотник/регулятор'] = False
     return ado
 
 def infos(file):
